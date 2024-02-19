d1dbe3bb7aa847913ba5f986a59142fa9dfc2a9f
CALeslie <lesliepride3+git@gmail.com>
2024-02-19 16:26:51 -0500

    Fixed Index.py

--- Lab2/src/Index.py
+++ Lab2/src/Index.py
@@ -2,4 +2,8 @@ import tornado.web
 
 class Handler(tornado.web.RequestHandler):
     def get(self):
-        self.write('<a href="/quote">Get a quote</a>')
+        self.write('User Profiles<br>')
+        self.write('<a href="/profile/alice">alice</a><br>')
+        self.write('<a href="/profile/bob">bob</a><br>')
+        self.write('<a href="/profile/carol">carol</a><br>')
+        self.write('<a href="/profile/dave">dave</a>')
